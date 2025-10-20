5b2afaf6458c3e144a4c102d64fdd392d211fdc3
MichaelEgli <michaelegli1879@gmail.com>
2025-10-20 14:48:22 +0200

    add CountryService Step2

--- ex1/src/CountryServiceStep2.java
+++ ex1/src/CountryServiceStep2.java
@@ -8,13 +8,15 @@ public class CountryServiceStep2 {
 
     public static void getCountryStep2(String code) throws IOException, InterruptedException {
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://translation-service-nameless-haze-4636.fly.dev/country/" + code))
-                .GET()
-                .build();
+        HttpResponse<String> response;
+        try (HttpClient client = HttpClient.newHttpClient()) {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://translation-service-nameless-haze-4636.fly.dev/country/" + code))
+                    .GET()
+                    .build();
 
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        }
         System.out.println("country step2 response: " + response.body());
 
     }
