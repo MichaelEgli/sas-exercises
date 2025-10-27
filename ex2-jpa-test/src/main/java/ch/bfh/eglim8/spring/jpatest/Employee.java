6e2353e1545c60a175fe2bb69f38da45b15cfdc7
MichaelEgli <michaelegli1879@gmail.com>
2025-10-27 10:51:49 +0100

    add exercise jpa-tests

---
+++ ex2-jpa-test/src/main/java/ch/bfh/eglim8/spring/jpatest/Employee.java
@@ -0,0 +1,39 @@
+package ch.bfh.eglim8.spring.jpatest;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+
+    @Id
+    private int id;
+
+    private String name;
+
+    private int salary;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
