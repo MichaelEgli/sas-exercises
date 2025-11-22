dbf8055be7c51bfb0263371d21fdedd30e1ed04f
MichaelEgli <michaelegli1879@gmail.com>
2025-11-22 11:55:38 +0100

    test HelloServiceTest

--- ex5-core-config/src/test/java/ch/bfh/eglim8/spring/hello/HelloServiceTest.java
+++ ex5-core-config/src/test/java/ch/bfh/eglim8/spring/hello/HelloServiceTest.java
@@ -1,10 +1,13 @@
 package ch.bfh.eglim8.spring.hello;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(classes = ConfigExerciseApplication.class)
 public class HelloServiceTest {
@@ -12,17 +15,25 @@ public class HelloServiceTest {
     @Autowired
     private HelloService helloService;
 
+    @Mock
+    private GreetingService greetingService;
+
     @Test
     public void testHelloService() {
         assertThat(helloService).isNotNull();
 
         String resultNewie = helloService.sayHello("Michael", 79);
-        System.out.println(resultNewie);
         assert resultNewie.contains("Newie");
 
         String resultOldie = helloService.sayHello("Roman", 80);
-        System.out.println(resultOldie);
-        assert resultNewie.contains("Oldie");
+        assert resultOldie.contains("Oldie");
+    }
+
+    @Test
+    public void testGreetingService() {
+        Mockito.when(greetingService.getGreeting()).thenReturn("gugus");
+        String resultNewie = helloService.sayHello("Michael", 79);
+        System.out.println(resultNewie);
     }
 
     @Test
