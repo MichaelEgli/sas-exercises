2f833e6cdbebdbfe75241330c094fdd1c538c7d6
Michael Egli <73283077+MichaelEgli@users.noreply.github.com>
2025-11-25 15:08:52 +0100

    Refactor CustomerService methods for readability

--- projektarbeit/application/order-feature-customer-service2/src/main/java/com/bfh/domi/order/services/CustomerService.java
+++ projektarbeit/application/order-feature-customer-service2/src/main/java/com/bfh/domi/order/services/CustomerService.java
@@ -3,6 +3,7 @@ package com.bfh.domi.order.services;
 import com.bfh.domi.order.dto.CustomerInfo;
 import com.bfh.domi.order.entity.Customer;
 import com.bfh.domi.order.repository.CustomerRepository;
+import java.util.Objects;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,11 +19,13 @@ public class CustomerService {
     }
 
     public Customer findCustomer(long id) throws CustomerNotFoundException {
-        return customerRepository.findById(id).orElseThrow(() -> new CustomerNotFoundException("Customer with id " + id + " not found"));
+        return customerRepository.findById(id).orElseThrow(
+            () -> new CustomerNotFoundException("Customer with id " + id + " not found"));
     }
 
     public List<CustomerInfo> searchCustomer(String name) {
-        return customerRepository.findAllByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(name, name);
+        return customerRepository.findAllByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(
+            name, name);
     }
 
     public Customer registerCustomer(Customer customer) {
@@ -33,5 +36,35 @@ public class CustomerService {
             return customerRepository.saveAndFlush(customer);
         }
     }
+
+    public Customer updateCustomer(Customer customer) {
+        Optional<Customer> customerExisting = customerRepository.findById(customer.getId());
+        List<CustomerInfo> emailExisting = customerRepository.findCustomersByEmail(customer.getEmail());
+        if (customerExisting.isPresent()) {
+            if (customerExisting.get().getEmail().equals(customer.getEmail())) {
+                return customerRepository.saveAndFlush(customer);
+            } else {
+                if (emailExisting.isEmpty()) {
+                    return customerRepository.saveAndFlush(customer);
+                } else {
+                    throw new EmailAlreadyExistsException("Email already exists");
+                }
+            }
+        } else {
+            throw new CustomerNotFoundException(
+                "Customer with id " + customer.getId() + " not found");
+        }
+    }
+/*        if (customerExisting.isPresent() && customerExisting.get().getEmail().equals(customer.getEmail())) {
+            return customerRepository.saveAndFlush(customer);
+        } else {
+            if (emailExisting.isPresent()) {
+                throw new EmailAlreadyExistsException("Email already exists");
+            } else {
+                throw new CustomerNotFoundException(
+                    "Customer with id " + customer.getId() + " not found");
+            }
+        }
+    }*/
 
 }
