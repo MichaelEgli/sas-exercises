aef8fc2dc58f0fa3671e4bc07b211a562557c6b0
MichaelEgli <michaelegli1879@gmail.com>
2025-12-17 15:11:40 +0100

    reminder solution

---
+++ loesungen-ex-lehrer/reminder-main/src/main/java/org/example/ReminderService.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
+@Service
+public class ReminderService {
+
+	public void scheduleReminder(Instant start, String text) {
+	}
+}
